1b0815bd78b49980318fcae024bb95fccdad03e4
kunaldec022002 <kunalingale72@gmail.com>
2025-08-15 13:39:25 +0530

    change btn color

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,29 +1,125 @@
-import React, { useEffect,useState } from 'react'
-import {EmployeeData} from '../Data/EmployeeData'
+import React, { useEffect, useState } from "react";
+import { EmployeeData } from "../Data/EmployeeData";
 
 const Home = () => {
-
   const [data, setData] = useState([]);
 
-  useEffect(() =>{
-    setData(EmployeeData)
-  },[]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState(0);
+  const [id, setId] = useState(0);
+  const [isupdate, setIsUpdate] = useState(false);
 
-  const handleEdit = (id) =>{
-    alert(id);
-  }
+  useEffect(() => {
+    setData(EmployeeData);
+  }, []);
 
-   const handleDelete = (id) =>{
-    if (id > 0 ) {
-      if(window.confirm("are you sure to delete this item")){
-        const dt = data.filter(item => item.id !==id);
+  const handleEdit = (id) => {
+    const dt = data.filter((item) => item.id === id);
+    if (dt !== undefined) {
+      setIsUpdate(true);
+      setId(id);
+      setFirstName(dt[0].firstName);
+      setLastName(dt[0].lastName);
+      setAge(dt[0].age);
+    }
+  };
+
+  const handleDelete = (id) => {
+    if (id > 0) {
+      if (window.confirm("are you sure to delete this item")) {
+        const dt = data.filter((item) => item.id !== id);
         setData(dt);
       }
     }
-  }
+  };
+
+  const handleSave = () => {};
+
+  const handleUpdate = () => {
+    const index = data
+      .map((item) => {
+        return item.id;
+      })
+      .indexOf(id);
+
+    const dt = [...data];
+    dt[index].firstName = firstName;
+    dt[index].lastName = lastName;
+    dt[index].age = age;
+
+    setData(dt);
+    handleClear();
+  };
+
+  const handleClear = () => {
+    setId(0);
+    setFirstName("");
+    setLastName("");
+    setAge("");
+    setIsUpdate(false);
+  };
+
   return (
     <div>
-      <table className='table table-hover'>
+      <h1 style={{ textAlign: "center", margin: "10px" }}>CRUD APPLICATION</h1>
+
+      <div style={{ display: "flex", justifyContent: "center",margin:"10px" }}>
+        <div>
+          <label>
+            first Name:
+            <input
+              type="text"
+              placeholder="enter first name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              placeholder="enter last name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            age:
+            <input
+              type="text"
+              placeholder="enter age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div style={{ marginLeft: "5px" }}>
+          {!isupdate ? (
+            <button className="btn btn-success" onClick={() => handleSave()}>
+              Save
+            </button>
+          ) : (
+            <button className="btn btn-success" onClick={() => handleUpdate()}>
+              Update
+            </button>
+          )}
+          &nbsp;
+          <button className="btn btn-danger" onClick={() => handleClear()}>
+            Clear
+          </button>
+          &nbsp;
+        </div>
+      </div>
+
+      <table className="table table-success table-striped-columns">
         <thead>
           <tr>
             <td>Sr.No</td>
@@ -36,27 +132,36 @@ const Home = () => {
         </thead>
 
         <tbody>
-          {
-           data.map((item, index) =>{
-            return(
+          {data.map((item, index) => {
+            return (
               <tr key={index}>
-                <td>{index+1}</td>
+                <td>{index + 1}</td>
                 <td>{item.id}</td>
                 <td>{item.firstName}</td>
                 <td>{item.lastName}</td>
                 <td>{item.age}</td>
                 <td>
-                  <button className='btn btn-primary' onClick={()=> handleEdit(item.id)}>Edit</button>&nbsp;
-                  <button className='btn btn-danger' onClick={()=> handleDelete(item.id)}>Delete</button>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleEdit(item.id)}
+                  >
+                    Edit
+                  </button>
+                  &nbsp;
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
-            )
-           })
-          }
+            );
+          })}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
